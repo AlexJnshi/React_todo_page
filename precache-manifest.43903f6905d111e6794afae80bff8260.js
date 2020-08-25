4a72353d349a5ddcb495e13ad27362d7b2ba72b9
Alex Shi <junnan.shi295@gmail.com>
2020-08-25 15:09:44 +1000

    no message

---
+++ precache-manifest.43903f6905d111e6794afae80bff8260.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0676ad77ed35c1ce66248f1f3f58083f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9c2b93ae4a67f766bc2c",
+    "url": "./static/css/main.f95d1e38.chunk.css"
+  },
+  {
+    "revision": "2c10559ad87bceb5f415",
+    "url": "./static/js/2.178908fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.178908fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c2b93ae4a67f766bc2c",
+    "url": "./static/js/main.51330f8c.chunk.js"
+  },
+  {
+    "revision": "f850bafd47a48cd1d2b1",
+    "url": "./static/js/runtime-main.5b8f4c02.js"
+  }
+]);
